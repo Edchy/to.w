d20a9226881156706ddbd31f6533b04e3f472e10
edch <soderblomed@gmail.com>
2026-05-04 11:18:29 +0200

    remove theme picker

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,4 +1,4 @@
-export const CONTACT_EMAIL = "tovewatte@gmail.com";
+export const CONTACT_EMAIL = "tove.watte@gmail.com";
 export const CONTACT_EMAIL_HREF = `mailto:${CONTACT_EMAIL}`;
 
 export const getProductSlug = (entryId: string) => entryId.replace(/\/data$/, "");
